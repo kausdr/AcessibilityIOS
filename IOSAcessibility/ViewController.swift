e44587bbca153f65618a8a6e3977f44adb7cdbe2
Kauane S. R. <kau@MacBook-Pro-de-Kauane.local>
2025-04-27 13:24:46 -0300

    adds voice over, acessibilities, high contrast and dynamic type

--- IOSAcessibility/ViewController.swift
+++ IOSAcessibility/ViewController.swift
@@ -11,37 +11,103 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         apagarTudo()
-      
+        
+        areaDeCalculo.accessibilityLabel = "Área de entrada"
+        areaDeCalculo.accessibilityHint = "Exibe seus números ou operações aqui"
+        areaDeCalculo.accessibilityTraits = .staticText
+        areaDeCalculo.font = UIFont.preferredFont(forTextStyle: .body)
+        areaDeCalculo.adjustsFontForContentSizeCategory = true
+        
+        areaDeResultado.accessibilityLabel = "Resultado"
+        areaDeResultado.accessibilityHint = "Exibe o resultado das operações aqui"
+        areaDeResultado.accessibilityTraits = .staticText
+        areaDeResultado.font = UIFont.preferredFont(forTextStyle: .body)
+        areaDeResultado.adjustsFontForContentSizeCategory = true
+        
+        adjustForAccessibilityAndTheme()
+        
     }
     
     var workings: String = ""
     
-    //inputs
-    @IBOutlet weak var input: UITextView!
+    //areaDeCalculos
+    
+    @IBOutlet weak var areaDeCalculo: UITextView!
     
     
-    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var areaDeResultado: UILabel!
+    
+    
+    
     
     
     // ====================================
     
     //Botões com rótulos descritivos para leitura de ferramentas de acessibilidade de acordo com a Human Interface Guidelines da Apple
     
+    //Uso de accessibilityLabel
+    //Uso de accessibilityHint
+    //Uso de UIAccessibilityTraits
+    
     // operações
     @IBAction func somar(_ sender: Any) {
         addToWorkings(value: "+")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Somar"
+            button.accessibilityHint = "Somar os números na calculadora"
+            button.accessibilityTraits = .button
+            button.setTitle("", for: .normal)
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+            
+        }
     }
     
     @IBAction func subtrair(_ sender: Any) {
         addToWorkings(value: "-")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Subtrair"
+            button.accessibilityHint = "Subtrair os números na calculadora"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+            
+        }
     }
     
     @IBAction func dividir(_ sender: Any) {
         addToWorkings(value: "%")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Dividir"
+            button.accessibilityHint = "Dividir os números na calculadora"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+            
+        }
     }
     
     @IBAction func multiplicar(_ sender: Any) {
         addToWorkings(value: "x")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Multiplicar"
+            button.accessibilityHint = "Multiplicar os números na calculadora"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+        }
     }
     
     
@@ -49,16 +115,46 @@ class ViewController: UIViewController {
         let expression = NSExpression(format: workings)
         let result = expression.expressionValue(with: nil, context: nil) as! Double
         let resultString = formatResult(result)
-        display.text = resultString
+        areaDeResultado.text = resultString
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Botão de resultado"
+            button.accessibilityHint = "Calcular o resultado da expressão na calculadora"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+        }
     }
     
     
     @IBAction func BotaoApagar(_ sender: Any) {
         apagar()
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Botão de apagar"
+            button.accessibilityHint = "Apagar o último número ou operação da calculadora"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+        }
     }
     
     @IBAction func BotaoLimparTudo(_ sender: Any) {
         apagarTudo()
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Botão de limpar"
+            button.accessibilityHint = "Limpar tudo o que está na calculadora"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+        }
     }
     
     
@@ -68,43 +164,136 @@ class ViewController: UIViewController {
     //números
     @IBAction func zero(_ sender: Any) {
         addToWorkings(value: "0")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número zero"
+            button.accessibilityHint = "Adicionar o número 0"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+        }
     }
     
     @IBAction func um(_ sender: Any) {
         addToWorkings(value: "1")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número um"
+            button.accessibilityHint = "Adicionar o número 1"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+        }
     }
     
     @IBAction func dois(_ sender: Any) {
         addToWorkings(value: "2")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número dois"
+            button.accessibilityHint = "Adicionar o número 2"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+            
+        }
     }
     
     
     @IBAction func tres(_ sender: Any) {
         addToWorkings(value: "3")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número três"
+            button.accessibilityHint = "Adicionar o número 3"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+        }
     }
     
     @IBAction func quatro(_ sender: Any) {
         addToWorkings(value: "4")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número quatro"
+            button.accessibilityHint = "Adicionar o número 4"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+        }
     }
     
     @IBAction func cinco(_ sender: Any) {
         addToWorkings(value: "5")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número cinco"
+            button.accessibilityHint = "Adicionar o número 5"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+        }
     }
     
     @IBAction func seis(_ sender: Any) {
         addToWorkings(value: "6")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número seis"
+            button.accessibilityHint = "Adicionar o número 6"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+        }
     }
     
     @IBAction func sete(_ sender: Any) {
         addToWorkings(value: "7")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número sete"
+            button.accessibilityHint = "Adicionar o número 7"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+        }
     }
     
     @IBAction func oito(_ sender: Any) {
         addToWorkings(value: "8")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número oito"
+            button.accessibilityHint = "Adicionar o número 8"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+        }
     }
     
     @IBAction func nove(_ sender: Any) {
         addToWorkings(value: "9")
+        
+        if let button = sender as? UIButton {
+            button.accessibilityLabel = "Número nove"
+            button.accessibilityHint = "Adicionar o número 9"
+            button.accessibilityTraits = .button
+            button.titleLabel?.font = UIFont.preferredFont(forTextStyle: .body)
+            button.titleLabel?.adjustsFontForContentSizeCategory = true
+            
+        }
     }
     
     // ====================================
@@ -114,21 +303,21 @@ class ViewController: UIViewController {
     
     //funções
     func apagarTudo() {
-        input.text = ""
+        areaDeCalculo.text = ""
         workings = ""
-        display.text = ""
+        areaDeResultado.text = ""
     }
     
     func addToWorkings(value: String) {
         workings = workings + value
-        input.text = workings
+        areaDeCalculo.text = workings
     }
     
-
+    
     func apagar() {
         if !workings.isEmpty {
             workings.removeLast()
-            input.text = workings
+            areaDeCalculo.text = workings
         }
     }
     
@@ -141,5 +330,33 @@ class ViewController: UIViewController {
         
     }
     
+    func adjustForAccessibilityAndTheme() {
+        // Detecta se o modo de contraste alto está ativado
+        if UIAccessibility.isDarkerSystemColorsEnabled {
+            view.backgroundColor = .black
+            areaDeCalculo.backgroundColor = .white
+            areaDeResultado.backgroundColor = .white
+            areaDeCalculo.textColor = .black
+            areaDeResultado.textColor = .black
+        } else {
+            // Modo padrão
+            view.backgroundColor = .white
+            areaDeCalculo.backgroundColor = .lightGray
+            areaDeResultado.backgroundColor = .lightGray
+            areaDeCalculo.textColor = .black
+            areaDeResultado.textColor = .black
+        }
+    }
+
+
+    
+    // Detectando mudanças no modo de interface
+        override func traitCollectionDidChange(_ previousTraitCollection: UITraitCollection?) {
+            super.traitCollectionDidChange(previousTraitCollection)
+            
+            adjustForAccessibilityAndTheme()
+        }
+    
 }
+
 
